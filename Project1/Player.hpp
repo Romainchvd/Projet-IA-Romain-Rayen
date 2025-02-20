6d6cba67b3ff0a3204edef897bf39477c5558920
Romain Dumont <rominou.dumont@gmail.com>
2025-02-20 12:54:24 +0100

    Réduction du délai de détection du GOAPEnemy

--- Project1/Player.hpp
+++ Project1/Player.hpp
@@ -10,7 +10,7 @@ public:
     FloatRect hitbox;
     FloatRect endOfDetectionArea;
     Clock detectionClock;
-    Time detectionCooldown = seconds(1);
+    Time detectionCooldown = seconds(0.4);
     static constexpr float SPEED = 200.0f;
     Player(float x, float y);
     void update(float deltaTime, Grid& grid) override;
